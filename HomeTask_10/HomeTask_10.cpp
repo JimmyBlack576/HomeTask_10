f58cbd65ebbd5eb972f6b956f8d972bade92b21e
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-21 00:19:51 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_10/HomeTask_10.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int input, min, min1, hour, hour1, per;
+    cout << "Введите время в секундах: \n";
+    cin >> input;
+    per = 28800 - input;
+    hour = per / 3600;
+    min = per % 3600;
+    min1 = min / 60;
+    cout << "До окончания рабочего дня осталось: \n";
+
+    if(hour>=0&&hour<1)
+        cout<< hour << " часов " << min1 << " минут ";
+    else if (hour == 1)
+        cout << hour << " час " << min1 << " минут ";
+    else if (hour > 1 && hour < 5)
+        cout << hour << " часа " << min1 << " минут ";
+    else if (hour >= 5 && hour <= 8)
+        cout << hour << " часов " << min1 << " минут ";
+
+
+    return 0;
+}
